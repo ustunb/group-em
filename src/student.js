f009c9f23561ea2998fd7fd70411f724deca7ca2
Berk Ustun <ustunb@mit.edu>
2016-04-11 19:08:16 -0400

    added new class to represent students

---
+++ src/student.js
@@ -0,0 +1,39 @@
+/* Depends on
+ *
+ * lodash.js
+ * group.js
+ * classroom.js
+ * 
+ */
+
+/**
+ * immutable class to represent each student
+ * @param {[int]} id   [unique student identifier; must be positive]
+ * @param {[string]} name [full student name, must only contain letters a-zA-Z or spaces/dash/apostophe and contain at least 1 non-whitespace character]
+ */
+function Student(name, id) {
+
+    var self = this;
+    if (!(_.isInteger(id) && id >= 0)) throw new Error('integer id required');
+    if (!_.isString(name)) throw new Error('name must be String');
+    name = name.trim();
+    if (name.length < 1) throw new Error('name must contain at least 1 non-whitespace character');
+    if (!(/^[a-zA-Z]+$/.test(name))) throw new Error('name can only contain a-z, A-Z, spaces, dashes and apostrophes');
+
+    //public methods
+    self.name = function() {
+        return name;
+    }
+
+    self.id = function() {
+        return id;
+    }
+
+    self.valueOf = function() {
+        return id;
+    }
+
+    self.toString = function() {
+        return name + ' (sid: ' + id + ')';
+    }
+}
