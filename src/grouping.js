55c6b7a42590f595894ac25a64ada475ac2935ac
Berk Ustun <ustunb@mit.edu>
2016-04-11 23:00:33 -0400

    added rep for Group, sloppy tests for Student + Group

--- src/grouping.js
+++ src/grouping.js
@@ -1,115 +1,151 @@
 /* Depends on
-*
-* group.js
-* classroom.js
-*
-*/
-function Grouping(classroom) {
-	
-	var self = this;
-	self.student_pool = []
-	self.random_groups = []
-	self.pinned_groups = [] 	//#explicitly enforced groups
-	self.banned_groups = [] 	//#explicitly banned groups
-	//
-	// Rep Invariants 
-	//
-	// every student has a unique student_id
-	// every group has a unique group_id
-	// every student is in exactly one group
-	// every group contains at least 1 student
-	// all groups = random_groups + pinned_groups
-	// all students = students in random_groups + students in pinned_groups
-	// random_groups and pinned_groups cannot contain any groups from banned_groups
-	// either random_groups.length = 0 or student_pool.length = 0
-	// only students in student_pool are 'shuffled' to form random_groups
+ *
+ * lodash.js
+ * student.js
+ */
 
-	//get number of groups in the current grouping
-	function size(){
-		return self.random_groups.length + self.pinned_groups.length + self.student_pool.length;
-	}
-	
-    //returns true iff this grouping has the same groups as that grouping
-    function equals(thatGrouping) {
-    	//TODO
+function Grouping(studentList, students_per_group) {
+
+    var self = this;
+    self.student_pool = [] //#pool of students in the classroom
+    self.random_groups = [] //#randomly generated groups
+    self.pinned_groups = [] //#explicitly enforced groups
+    self.banned_groups = [] //#explicitly banned groups
+    self.students_per_group = students_per_group
+        //
+        // Rep Invariants 
+        //
+        // every student has a unique student_id
+        // every group has a unique group_id
+        // every student is in exactly one group
+        // every group contains at least 1 student
+        // all groups = random_groups + pinned_groups
+        // all students = students in random_groups + students in pinned_groups
+        // random_groups and pinned_groups cannot contain any groups from banned_groups
+        // either random_groups.length = 0 or student_pool.length = 0
+        // only students in student_pool are 'shuffled' to form random_groups
+        // 
+        // 1 <= students_per_group <= classroom.getNumberOfStudents
+
+    //get number of groups in the current grouping
+    function getNumberOfGroups() {
+        return self.random_groups.length + self.pinned_groups.length
     }
-    
-	//returns list of groups in current grouping
-	function toString() {
-		//TODO
-	}
-	
-	//check representation invariants
-	function checkRep() {
-		//TODO
-		return true;
-	}
 
-	//returns group_id of group with a given student
-	function groupOf(student_id){
-		//TODO
-	}
+    //get the number of students in the current grouping
+    function getNumberOfStudents() {
+        return self.student_pool.length +
+            _.reduce(self.random_groups, function(s, g) {
+                return s + g.size();
+            }, 0) +
+            _.reduce(self.pinned_groups, function(s, g) {
+                return s + g.size();
+            }, 0)
+    }
 
-	//add group to current grouping
-	function addGroup(group_id){
-	}
+    //returns true iff this grouping has the same student pool, random groups, pinned groups, banned groups (regardless of ordering)
+    function equals(thatGrouping) {
+        return arraysEqual(self.student_pool, thatGrouping.student_pool) &&
+            arraysEqual(self.random_groups, thatGrouping.random_groups) &&
+            arraysEqual(self.pinned_groups, thatGrouping.pinned_groups) &&
+            arraysEqual(self.banned_groups, thatGrouping.banned_groups)
+    }
 
-	//remove group from current grouping
-	function removeGroup(group_id){
-		//TODO
-	}
+    //returns list of groups in current grouping
+    function toString() {
+        //TODO
+    }
 
-	//switch student from one old group to new group
-	function switchGroups(student_id, old_group_id, new_group_id){
-		//TODO
-	}
+    //returns group that contains a given student
+    function groupOf(student_id) {
+        //TODO
+    }
 
-	//randomly create new groups of students
-	function shuffle(){
-		//TODO
-	}
+    //check representation invariants
+    function checkRep() {
+        if (!_.isInteger(self.students_per_group)) throw new Exception('students per group must be integer')
+        if (self.students_per_group < 1.0) throw new Exception('at least 1 student per group is needed');
+        if (self.students_per_group > self.getNumberOfStudents()) throw new Exception('students per group larger than total students');
+        return true;
+    }
 
-	//pins a group
-	function pinGroup(group_id){
-		var pin_flag = false;
-		//TODO
-		return pin_flag;
-	}
-	
-	//unpins a group
-	function unpinGroup(group_id){
-		var unpin_flag = false;
-		//TODO
-		return unpin_flag;
-	}
+    //add a new group to random_groups using students in the student_pool
+    function addGroup(student_ids) {
+        //TODO
+        checkRep();
+    }
 
-	//adds group to the list of banned groups
-	function banGroup(group_id){
-		var ban_flag = false;
-		//TODO
-		return ban_flag;
-	}
+    //remove group from random_groups and add students back to the student_pool
+    function removeGroup(group) {
+        //TODO
+        checkRep();
+    }
 
-	//remove group from the list of banned groups
-	function unbanGroup(group_id){
-		var unban_flag = false;
-		//TODO
-		return unban_flag;
-	}
-	
-	//attach functions to object
-	self.size = size
-	self.equals = equals
-	self.toString = toString
+    //switch student from one group to another group
+    function switchGroups(student_id, old_group_id, new_group_id) {
+        if (old_group.hasStudent(student_id)) {
+            old_group.removeStudent(student_id)
+            new_group.addStudent(student_id)
+        }
+        checkRep();
+    }
 
-	self.groupOf = groupOf
-	self.addGroup = addGroup
-	self.removeGroup = removeGroup
-	self.switchGroups = switchGroups
-	self.shuffle = shuffle
-	self.pinGroup = pinGroup
-	self.unpinGroup = unpinGroup
-	self.banGroup = forbidGroup
-	self.unbanGroup = unforbidGroup
-	
-}
+    //randomly create new groups of students from student_pool
+    function shuffle() {
+        //TODO
+        //shuffle
+        //_.shuffle(self.random_groups)
+        //_.chunk(array, [size=1])
+        checkRep();
+        //student_pool should now be empty
+    }
+
+    //adds a student group to the list of pinned groups
+    function pinGroup(group) {
+        var pin_flag = false;
+        //TODO
+        checkRep();
+        return pin_flag;
+    }
+
+    //removes a group from the list of pinned groups
+    function unpinGroup(group) {
+        var unpin_flag = false;
+        //TODO
+        checkRep();
+        return unpin_flag;
+    }
+
+    //adds group to the list of banned groups
+    function banGroup(group) {
+        var ban_flag = false;
+        //TODO
+        checkRep();
+        return ban_flag;
+    }
+
+    //remove group from the list of banned groups
+    function unbanGroup(group) {
+        var unban_flag = false;
+        //TODO
+        checkRep();
+        return unban_flag;
+    }
+
+
+    //attach functions to object
+    self.size = size
+    self.equals = equals
+    self.toString = toString
+
+    self.groupOf = groupOf
+    self.addGroup = addGroup
+    self.removeGroup = removeGroup
+    self.switchGroups = switchGroups
+    self.shuffle = shuffle
+    self.pinGroup = pinGroup
+    self.unpinGroup = unpinGroup
+    self.banGroup = forbidGroup
+    self.unbanGroup = unforbidGroup
+
+} * /
