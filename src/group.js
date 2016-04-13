7caacf8da05e716833d50ce4961d126c401835ce
Berk Ustun <ustunb@mit.edu>
2016-04-13 00:23:28 -0400

    implemented Group and Grouping and added descriptions (no tests yet)

--- src/group.js
+++ src/group.js
@@ -2,16 +2,10 @@
  *
  * lodash.js
  * student.js
- * 
+ *
  */
-//http://stackoverflow.com/questions/36558813/how-to-create-a-set-of-user-defined-objects-in-javascript-with-a-user-defined-eq?noredirect=1#comment60719822_36558813
 
-
-/**
- * [mutable class to represent a unique unordered collection of one or more students]
- * @param {[Array<Student>]} students [array composed of distinct students]
- */
-function Group(studentList) {
+function Group(studentArray) {
 
     // Rep Invariants
     // size >= 0
@@ -20,43 +14,46 @@ function Group(studentList) {
 
     //private fields
     var students = [];
-    var size = 0;     
-    var value = "0";//store value in cache
+    var studentIDs = [];
+    var size = 0;
+    var value = "0"; //store value in cache
 
     // checks to see if student is in list of students
-    function has(student_id) {
-        return _.map(students, function(v) {return v.id();}).indexOf(student_id) > -1
+    function has(studentID) {
+        return studentIDs.indexOf(studentID) > -1
     }
 
     //returns unique identifier for this group (group = [1,2,5] <=> group_ID = "10011")
     function valueOf() {
-        console.log('called valueOf')
-        return _.reduce(students, function(bin, s) {return bin + Math.pow(2, s.id() - 1);}, 0).toString(2);
+        return _.reduce(students, function(bin, s) {
+            return bin + Math.pow(2, s.id() - 1);
+        }, 0).toString(2);
     }
 
     // adds a new student to this group
     // returns true if student is successfully added to list of students
     function add(student) {
         var update_flag = false;
-        var student_id = student.valueOf();
+        var studentID = student.id();
         if (!self.has(student_id)) {
             students.push(student);
-            console.log(students)
+            studentIDs.push(studentID);
+            value += Math.pow(2, studentID);
             size++;
-            value = valueOf();
             update_flag = true;
             checkRep();
         }
         return update_flag;
     }
 
-    // gets a student from this group 
-    // returns null if student does not exist in this group
-    function get(student_id) {
+    // removes a student from this group 
+    function remove(studentID) {
         if (self.has(student_id)) {
-            student = students.get(student_id);
+            idx = student_ids.indexOf(studentID);
+            student = students.splice(idx, 1);
+            studentIDs.splice(idx, 1);
+            value -= Math.pow(2, studentID);
             size--;
-            value = valueOf();
             checkRep();
             return student;
         }
@@ -64,7 +61,9 @@ function Group(studentList) {
 
     //returns list of groups in current grouping
     function toString() {
-        return _.map(students, function(v) {return v.toString();}).join("\n");
+        return _.map(students, function(v) {
+            return v.toString();
+        }).join("\n");
     }
 
     //returns true iff this self has the same students as thatGroup    
@@ -78,23 +77,35 @@ function Group(studentList) {
     }
 
     //attach public methods to object
-    self.students = students
-    self.has = has
-    self.add = add
-    self.get = get
+    self.has = has;
+    self.add = add;
+    self.remove = get;
     self.equals = equals
     self.toString = toString
     self.valueOf = function() {
         return value;
     }
-    self.size = function() {
+
+    self.getSize = function() {
         return size;
     }
 
+    self.getStudents = function() {
+        return _.clone(students);
+    }
+
+    self.getGroupID = function() {
+        return Math.min(studentIDs);
+    }
+
+    self.getStudentIDs = function() {
+        return _.clone(studentIDs);
+    }
+
     //initialization
-    for (var i = 0; i < studentList.length; i++) {
-        if (!self.add(studentList[i])) {
-            throw new Error('student list should be distinct');
+    for (var i = 0; i < studentArray.length; i++) {
+        if (!self.add(studentArray[i])) {
+            throw new Error('student array should contain distinct students');
         }
     }
     checkRep();
--- src/grouping.js
+++ src/grouping.js
@@ -2,30 +2,53 @@
  *
  * lodash.js
  * student.js
+ * group.js
+ *
  */
 
-function Grouping(studentList, students_per_group) {
+//creating a new Grouping
+var grouping = new Grouping(classroom, students_per_group);
+
+//getting node / edge information for D3
+var groupArray = grouping.getGroups();
+for (var j = 0; j < groupArray.length; j++) {
+    var group = groupArray[j];
+    var studentArray = group.getStudents();
+    for (var k = 0; k < studentArray.length; k++) {
+        //set node information here
+    }
+}
+
+// remove a student from a group, then add them to another group
+// given student_id and group_id (group_id = lowest student id of all students in that group)
+grouping.addStudentToGroup(student_id, group_id);
+
+
+//remove student from a group, then drop them in the middle of nowhere
+//given student_id
+grouping.addStudentToGroup(student_id);
+
+
+//pinning a group (i.e. making sure it is kept for the shuffle)
+//TODO
+
+//unpinning a group (i.e., freeing it to change in the next shuffle)
+//TODO
+
+//banning a group (i.e., freeing it to change in the next shuffle)
+//TODO
+
+//unbanning a group (i.e., freeing it to change in the next shuffle)
+//TODO
+
+
+function Grouping(classroom, students_per_group) {
 
     var self = this;
-    self.student_pool = [] //#pool of students in the classroom
-    self.random_groups = [] //#randomly generated groups
-    self.pinned_groups = [] //#explicitly enforced groups
-    self.banned_groups = [] //#explicitly banned groups
-    self.students_per_group = students_per_group
-        //
-        // Rep Invariants 
-        //
-        // every student has a unique student_id
-        // every group has a unique group_id
-        // every student is in exactly one group
-        // every group contains at least 1 student
-        // all groups = random_groups + pinned_groups
-        // all students = students in random_groups + students in pinned_groups
-        // random_groups and pinned_groups cannot contain any groups from banned_groups
-        // either random_groups.length = 0 or student_pool.length = 0
-        // only students in student_pool are 'shuffled' to form random_groups
-        // 
-        // 1 <= students_per_group <= classroom.getNumberOfStudents
+    random_groups = _.chunk(classroom.getStudentList(), students_per_group); //randomly generated groups
+    students_per_group = students_per_group; //number of students per pool
+    pinned_groups = []; //explicitly enforced groups
+    //banned_groups = []; //explicitly banned groups
 
     //get number of groups in the current grouping
     function getNumberOfGroups() {
@@ -34,118 +57,150 @@ function Grouping(studentList, students_per_group) {
 
     //get the number of students in the current grouping
     function getNumberOfStudents() {
-        return self.student_pool.length +
-            _.reduce(self.random_groups, function(s, g) {
-                return s + g.size();
-            }, 0) +
-            _.reduce(self.pinned_groups, function(s, g) {
-                return s + g.size();
-            }, 0)
+        return _.reduce(random_groups, function(s, g) {
+            return s + g.getSize();
+        }, 0) + _.reduce(pinned_groups, function(s, g) {
+            return s + g.getSize();
+        }, 0)
     }
 
-    //returns true iff this grouping has the same student pool, random groups, pinned groups, banned groups (regardless of ordering)
-    function equals(thatGrouping) {
-        return arraysEqual(self.student_pool, thatGrouping.student_pool) &&
-            arraysEqual(self.random_groups, thatGrouping.random_groups) &&
-            arraysEqual(self.pinned_groups, thatGrouping.pinned_groups) &&
-            arraysEqual(self.banned_groups, thatGrouping.banned_groups)
+    //get the group ID of student in 
+    function getGroupIDOf(student_id) {
+        for (var i = 0; i < random_groups.length; i++) {
+            if (random_groups[i].has(student_id)) {
+                return random_groups[i].getGroupID();
+            }
+        }
+        for (var i = 0; i < pinned_groups.length; i++) {
+            if (pinned_groups[i].has(student_id)) {
+                return pinned_groups[i].getGroupID();
+            }
+        }
     }
 
-    //returns list of groups in current grouping
-    function toString() {
-        //TODO
+    //get the index  ID of student in 
+    function getGroupIndex(group_id) {
+        for (var i = 0; i < random_groups.length; i++) {
+            if (random_groups[i].has(group_id)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
-    //returns group that contains a given student
-    function groupOf(student_id) {
-        //TODO
+    //get a copy of the groups
+    function getGroups() {
+        checkRep();
+        return _.concat(random_groups, pinned_groups);
     }
 
-    //check representation invariants
+    //get a copy of the pinned groups
+    function getPinnedGroups() {
+        checkRep();
+        return _.clone(pinned_groups);
+    }
+
+    //get a copy of the random groups
+    function getRandomGroups() {
+        checkRep();
+        return _.clone(random_groups);
+    }
+
+    //checks representation invariants
     function checkRep() {
-        if (!_.isInteger(self.students_per_group)) throw new Exception('students per group must be integer')
-        if (self.students_per_group < 1.0) throw new Exception('at least 1 student per group is needed');
-        if (self.students_per_group > self.getNumberOfStudents()) throw new Exception('students per group larger than total students');
+        if (!_.isInteger(students_per_group)) throw new Exception('students per group must be integer')
+        if (students_per_group < 1.0) throw new Exception('at least 1 student per group is needed');
+        if (students_per_group > self.getNumberOfStudents()) throw new Exception('students per group larger than total students');
         return true;
     }
 
-    //add a new group to random_groups using students in the student_pool
-    function addGroup(student_ids) {
-        //TODO
-        checkRep();
-    }
-
-    //remove group from random_groups and add students back to the student_pool
-    function removeGroup(group) {
-        //TODO
-        checkRep();
-    }
-
     //switch student from one group to another group
-    function switchGroups(student_id, old_group_id, new_group_id) {
-        if (old_group.hasStudent(student_id)) {
-            old_group.removeStudent(student_id)
-            new_group.addStudent(student_id)
+    function addStudentToGroup(student_id, group_id) {
+        var old_group_id = getGroupIDOf(student_id);
+        var old_idx = getGroupIndex(old_group_id);
+        var student = random_groups[old_idx].remove(student_id)
+
+        //add student to new group
+        if (group_id == null) {
+            var new_idx = getGroupIndex(group_id);
+            random_groups.push(new Group(student));
+        } else {
+            var new_idx = getGroupIndex(group_id);
+            random_groups[new_idx].add(student);
+        }
+
+        //delete group from random groups;
+        if (random_groups[old_idx].getSize() == 0) {
+            random_groups.splice(old_idx, 1);
         }
         checkRep();
     }
 
     //randomly create new groups of students from student_pool
     function shuffle() {
-        //TODO
-        //shuffle
-        //_.shuffle(self.random_groups)
-        //_.chunk(array, [size=1])
-        checkRep();
-        //student_pool should now be empty
+        //populate student pool
+        //_.flatten(random_groups.map(function(group){return group.getStudents()}));
+        var studentArray = []
+        for (var i = 0; i < random_groups.length; i++) {
+            studentArray.push(random_groups[i].getStudents());
+        }
+        random_groups = _.shuffle(studentArray).map(function(students) {
+            return new Group(students);
+        })
+        return _.concat(random_groups, pinned_groups);
     }
-
-    //adds a student group to the list of pinned groups
-    function pinGroup(group) {
-        var pin_flag = false;
-        //TODO
-        checkRep();
-        return pin_flag;
-    }
-
-    //removes a group from the list of pinned groups
-    function unpinGroup(group) {
-        var unpin_flag = false;
-        //TODO
-        checkRep();
-        return unpin_flag;
-    }
-
-    //adds group to the list of banned groups
-    function banGroup(group) {
-        var ban_flag = false;
-        //TODO
-        checkRep();
-        return ban_flag;
-    }
-
-    //remove group from the list of banned groups
-    function unbanGroup(group) {
-        var unban_flag = false;
-        //TODO
-        checkRep();
-        return unban_flag;
-    }
-
 
     //attach functions to object
-    self.size = size
-    self.equals = equals
-    self.toString = toString
+    self.shuffle = shuffle;
+    self.addStudentToGroup = addStudentToGroupOf;
+    self.getGroups = getGroups;
+    self.getPinnedGroups = getPinnedGroups;
+    self.getRandomGroups = getRandomGroups;
+    self.getGroupIndex = getGroupIndex;
+    self.getNumberOfGroups = getNumberOfGroups;
+    self.getNumberOfStudents = getNumberOfStudents;
+    //self.pinGroup = pinGroup
+    //self.unpinGroup = unpinGroup
+    //self.banGroup = banGroup
+    //self.unbanGroup = unbanGroup
 
-    self.groupOf = groupOf
-    self.addGroup = addGroup
-    self.removeGroup = removeGroup
-    self.switchGroups = switchGroups
-    self.shuffle = shuffle
-    self.pinGroup = pinGroup
-    self.unpinGroup = unpinGroup
-    self.banGroup = forbidGroup
-    self.unbanGroup = unforbidGroup
+    self.shuffle();
+}
 
-} * /
+
+// //adds a student group to the list of pinned groups
+// function pinGroup(group) {
+//     var pin_flag = false;
+//     //TODO
+//     checkRep();
+//     return pin_flag;
+// }
+
+// //removes a group from the list of pinned groups
+// function unpinGroup(group) {
+//     var unpin_flag = false;
+//     //TODO
+//     checkRep();
+//     return unpin_flag;
+// }
+
+// //adds group to the list of banned groups
+// function banGroup(group) {
+//     var ban_flag = false;
+//     //TODO
+//     checkRep();
+//     return ban_flag;
+// }
+
+// //remove group from the list of banned groups
+// function unbanGroup(group) {
+//     var unban_flag = false;
+//     //TODO
+//     checkRep();
+//     return unban_flag;
+// }
+
+// //returns list of groups in current grouping
+// function toString() {
+//     //TODO
+// }
