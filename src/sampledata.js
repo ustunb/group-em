6f8bb94e3a77728872a4bec3c2c9f07464651e9d
David Bau <david.bau@gmail.com>
2016-04-29 10:00:02 -0400

    Add sample data

--- src/sampledata.js
+++ src/sampledata.js
@@ -1,90 +1,94 @@
 window.world = {
 
-  classroom: {
-    classname: "First Period Math",
-    students: [
-      {
-        name: "Alice",
-        prefer: [ "Bob" ],
-        avoid: [],
-        location: null
-      },
-      {
-        name: "Bob",
-        prefer: [ ],
-        avoid: [ "Alice" ],
-        location: null
-      },
-      {
-        name: "Cory",
-        prefer: [ ],
-        avoid: [ ],
-        location: {x: 123, y: 123}
-      },
-      {
-        name: "Dana",
-        prefer: [ ],
-        avoid: [ "Bob" ],
-        location: {x: 523, y: 323}
-      }
-    ],
-    groupsize: 2,
-    groups: [
-      {
-        members: ["Alce", "Bob"]
-        location: {x: 523, y: 123 }
-      },
-      {
-        members: ["Cory", "Dana"]
-        location: {x: 223, y: 252 }
-      }
-    ],
-  },
+  currentroom: 0,
 
-  classroom: {
-    classname: "Second Period Math",
-    students: [
-      {
-        name: "Elo",
-        prefer: [  ],
-        avoid: [],
-        location: null
-      },
-      {
-        name: "Fra",
-        prefer: [ ],
-        avoid: [  ],
-        location: null
-      },
-      {
-        name: "Gem",
-        prefer: [ ],
-        avoid: [ ],
-        location: null
-      },
-      {
-        name: "Har",
-        prefer: [ ],
-        avoid: [  ],
-        location: null
-      },
-      {
-        name: "Ira",
-        prefer: [ ],
-        avoid: [  ],
-        location: null
-      }
-    ],
-    groupsize: 3,
-    groups: [
-      {
-        members: ["Elo", "Gem", "Har"]
-        location: null
-      },
-      {
-        members: ["Fra", "Ira"]
-        location: null
-      }
-    ],
-  }
+  classrooms: [ 
+    {
+      classname: "First Period Math",
+      students: [
+        {
+          name: "Alice",
+          prefer: [ "Bob" ],
+          avoid: [],
+          location: null
+        },
+        {
+          name: "Bob",
+          prefer: [ ],
+          avoid: [ "Alice" ],
+          location: null
+        },
+        {
+          name: "Cory",
+          prefer: [ ],
+          avoid: [ ],
+          location: {x: 123, y: 123}
+        },
+        {
+          name: "Dana",
+          prefer: [ ],
+          avoid: [ "Bob" ],
+          location: {x: 523, y: 323}
+        }
+      ],
+      groupsize: 2,
+      groups: [
+        {
+          members: ["Alce", "Bob"],
+          location: {x: 523, y: 123 }
+        },
+        {
+          members: ["Cory", "Dana"],
+          location: {x: 223, y: 252 }
+        }
+      ],
+    },
+  
+    {
+      classname: "Second Period Math",
+      students: [
+        {
+          name: "Elo",
+          prefer: [  ],
+          avoid: [],
+          location: null
+        },
+        {
+          name: "Fra",
+          prefer: [ ],
+          avoid: [  ],
+          location: null
+        },
+        {
+          name: "Gem",
+          prefer: [ ],
+          avoid: [ ],
+          location: null
+        },
+        {
+          name: "Har",
+          prefer: [ ],
+          avoid: [  ],
+          location: null
+        },
+        {
+          name: "Ira",
+          prefer: [ ],
+          avoid: [  ],
+          location: null
+        }
+      ],
+      groupsize: 3,
+      groups: [
+        {
+          members: ["Elo", "Gem", "Har"],
+          location: null
+        },
+        {
+          members: ["Fra", "Ira"],
+          location: null
+        }
+      ]
+    }
+  ]
 }
